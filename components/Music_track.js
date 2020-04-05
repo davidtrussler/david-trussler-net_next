c12e97c2a4d0f59ba6966a2c2a00400af7af0d67
David Trussler <mail@davidtrussler.net>
2020-04-05 16:59:09 +0100

    Adds Music_track component

---
+++ components/Music_track.js
@@ -0,0 +1,20 @@
+export function Music_track() {
+	return (
+		<div className="block" id="seinix">
+			<h3>seinix</h3>
+
+			<h4>electronics</h4>
+
+			<p>
+				<img src="images/music/tracks/seinix.gif" className="pic"/>
+
+				After creating <span className="namedTitle">foniq</span>, which was a remix from supplied sound samples, I decided to recreate its spirit in a piece that used completely original sounds. This collection of clicks and bleeps is the result.
+			</p>
+
+			<audio controls>
+				<source src="http://www.zen146343.zen.co.uk/davidtrussler/audio/seinix.mp3" type="audio/mpeg"/>
+				<source src="http://www.zen146343.zen.co.uk/davidtrussler/audio/seinix.ogg" type="audio/ogg; codecs=&quot;vorbis&quot;"/> 
+			</audio> 
+		</div>
+	)
+}
