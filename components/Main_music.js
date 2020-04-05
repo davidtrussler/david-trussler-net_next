2662a9f59a89487205fbdd4314cf60f0f34c64a9
David Trussler <mail@davidtrussler.net>
2020-04-05 17:15:29 +0100

    Updates Main_music component

--- components/Main_music.js
+++ components/Main_music.js
@@ -1,8 +1,6 @@
 import {Music_heading} from '../components/Music_heading'; 
 import {Music_intro} from '../components/Music_intro'; 
 import {Music_tracks} from '../components/Music_tracks'; 
-import {Music_intro} from '../components/Music_intro'; 
-import {Music_tracks} from '../components/Music_tracks'; 
 
 export function Main_music() {
 	return (
@@ -10,8 +8,6 @@ export function Main_music() {
 	    <div className="constrained">
 	      <div className="main__inner">
 	      	<Music_heading/>
-	      	<Music_intro/>
-	      	<Music_tracks/>
 	      	<Music_intro/>
 	      	<Music_tracks/>
 	      </div>
