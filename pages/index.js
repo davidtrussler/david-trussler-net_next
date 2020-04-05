b89fa5ff4ea384f7d52b9664af6a1a4de6a56476
David Trussler <mail@davidtrussler.net>
2020-04-05 11:51:50 +0100

    Updates <head> element - adds <title> - adds viewport metatag - adds favicon

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import {Header} from '../components/Header'; 
 import {Main} from '../components/Main'; 
 import {Footer} from '../components/Footer'; 
@@ -6,6 +7,11 @@ import '../stylesheets/app.scss';
 function App() {
   return (
     <React.Fragment>
+      <Head>
+        <title>david trussler</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="icon" type="image/jpg" href="/images/favicon.png"/>
+      </Head>
       <Header/>
       <Main/>
       <Footer/>
