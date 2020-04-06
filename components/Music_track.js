2d712c22088011285f7e9bd54c77f526142b2c57
David Trussler <mail@davidtrussler.net>
2020-04-06 23:09:42 +0100

    Updates Music_track to create filename variable from title

--- components/Music_track.js
+++ components/Music_track.js
@@ -1,4 +1,6 @@
 export function Music_track(props) {
+	let filename = props.title.replace(/ /g, ''); 
+
 	return (
 		<section className="music__track">
 			<h3>{props.title}</h3>
@@ -11,15 +13,13 @@ export function Music_track(props) {
 			}
 			</ul>
 
-			<img src={`images/music/tracks/${props.title}.gif`}/>
+			<img src={`images/music/tracks/${filename}.gif`}/>
 
 			<p>{props.text}</p>
 
-			<p>{props.text_2}</p>
-
 			<audio controls>
-				<source src={`http://www.zen146343.zen.co.uk/davidtrussler/audio/${props.title}.mp3`} type="audio/mpeg"/>
-				<source src={`http://www.zen146343.zen.co.uk/davidtrussler/audio/${props.title}.ogg`} type="audio/ogg; codecs=&quot;vorbis&quot;"/> 
+				<source src={`http://www.zen146343.zen.co.uk/davidtrussler/audio/${filename}.mp3`} type="audio/mpeg"/>
+				<source src={`http://www.zen146343.zen.co.uk/davidtrussler/audio/${filename}.ogg`} type="audio/ogg; codecs=&quot;vorbis&quot;"/> 
 			</audio> 
 		</section>
 	)
